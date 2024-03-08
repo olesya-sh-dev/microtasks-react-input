be47e7c4a2bf63b0fd54230d2ee0f67f1d36af0a
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-03-08 23:50:20 +0300

    добавление uuid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
-
 import "./App.css";
 // import { FullInput } from "./components/FullInput";
 import { Input } from "./components/Input";
 import { Button } from "./components/Button";
+import { v1 } from "uuid";
 
 function App() {
   let [message, setMessage] = useState([
-    { message: "message1" },
-    { message: "message2" },
-    { message: "message3" },
-    { message: "message4" },
-    { message: "message5" },
+    { message: "message1", id: v1() },
+    { message: "message2", id: v1() },
+    { message: "message3", id: v1() },
+    { message: "message4", id: v1() },
+    { message: "message5", id: v1() },
   ]);
   let [title, setTitle] = useState("");
   console.log(title);
   //создаем функцию, которая будет добавлять задачу в список
   const addMessage = (title: string) => {
     // console.log(title);
-    let newMessage = { message: title };
+    let newMessage = { message: title, id: v1() };
     setMessage([newMessage, ...message]);
   };
 
@@ -36,8 +36,12 @@ function App() {
         <input />
         <button>+</button>
       </div> */}
-      {message.map((el, index) => {
-        return <div key={index}>{el.message}</div>;
+      {message.map((el, id) => {
+        return (
+          <div key={id}>
+            {el.message},{el.id}
+          </div>
+        );
       })}
     </div>
   );
